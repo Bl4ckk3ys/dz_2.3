bd13b996c51a86a87518d775ca464b590ff9d10b
bl4ckk3ys <ilchechnev@gmail.com>
2023-10-11 13:13:47 +0300

    New commit

--- dz_2_3.c
+++ dz_2_3.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 int main()
 {   
-    int x, y, null;
-    
+    int x, y, null;  
     scanf("%d %d", &x, &y);
+    
     if (x>0 && y>0) printf("First quarter");
     else if (x<0 && y>0) printf("Second quarter");
     else if (x<0 && y<0) printf("Third quarter");
@@ -14,7 +12,7 @@ int main()
     else if (x==0 && y==0) printf("Origin of coordinates");
     else if (x==0) printf("Y axis");
     else if (y==0) printf("X axis");
-    
+
     scanf("\n%d",&null);
     return 0;
 }
